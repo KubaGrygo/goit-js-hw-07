abde68bb051d9da05af3c0ea89b874cd4fa9302f
KubaGrygo <117944751+KubaGrygo@users.noreply.github.com>
2023-05-10 20:33:10 +0200

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,30 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryEl = document.querySelector(".gallery");
+
+for (const image of galleryItems) {
+  const listEl = `<div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src=${image.preview}
+        data-source=${image.original}
+        alt=${image.description}
+      />
+    </a>
+  </div>`;
+
+  galleryEl.insertAdjacentHTML("beforeend", listEl);
+  galleryEl.addEventListener("click", (e) => e.preventDefault());
+}
+
+galleryEl.addEventListener("click", (e) => {
+  const instance = basicLightbox.create(`<img src=${e.target.dataset.source}>`);
+  instance.show();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" || "Space") {
+      instance.close();
+    }
+  });
+});
---
+++ js/02-lightbox.js
@@ -0,0 +1,21 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+const makeGallery = () => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+              </a>`;
+    })
+    .join("");
+};
+
+galleryEl.innerHTML = makeGallery();
+
+const galleryInstance = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
